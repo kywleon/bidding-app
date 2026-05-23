a21c85596f15295c06732d252936bcd5c3327ee3
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2026-05-23 12:34:20 +0800

    Update the websocker service from reverb to pusher

--- frontend/src/composables/useWebSocket.js
+++ frontend/src/composables/useWebSocket.js
@@ -10,14 +10,16 @@ function getEcho() {
   window.Pusher = Pusher;
 
   echoInstance = new Echo({
-    broadcaster: "reverb",
-    key: import.meta.env.VITE_REVERB_APP_KEY ?? "bidding-app-key",
-    wsHost: import.meta.env.VITE_REVERB_HOST ?? "localhost",
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "http") === "https",
-    enabledTransports: ["ws", "wss"],
-    disableStats: true,
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY ?? "bidding-app-key",
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    // wsHost: import.meta.env.VITE_REVERB_HOST ?? "localhost",
+    // wsPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    // wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    // forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "http") === "https",
+    // enabledTransports: ["ws", "wss"],
+    // disableStats: true,
+    forceTLS: true,
   });
 
   return echoInstance;
